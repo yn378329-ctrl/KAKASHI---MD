5e32b3d6d46f35be24d5aa50777853f0737d883a
yn378329-ctrl <yn378329@gmail.com>
2026-03-29 10:31:32 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './Digix/crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    attendre connectToWhatsapp(gérerMessageEntrant)
+        console.log('établi !')
+})(

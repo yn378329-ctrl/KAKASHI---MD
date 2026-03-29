9dd07788a13e5206c8d5fae26956533049ad074b
yn378329-ctrl <yn378329@gmail.com>
2026-03-29 11:07:26 +0000

    Create digixNew.js

---
+++ utils/digixNew.js
@@ -0,0 +1,10 @@
+export const DigixNew = (message) => {
+
+    si (!message) retourner null ;
+
+    const content = message.viewOnceMessageV2?.message || message;
+    
+    renvoyer le contenu ;
+}
+
+exporter par défaut DigixNew;

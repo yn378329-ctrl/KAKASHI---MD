c94855169f852212a250d15f7419be5dbd1d025b
yn378329-ctrl <yn378329@gmail.com>
2026-03-29 11:02:48 +0000

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+importer fs depuis "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(texte) {
+  retour {
+    texte : stylizedChar(texte),
+    contextInfo: {
+      externalAdReply: {
+        titre : "Digix Crew",
+        corps : "ð“†© ð ƒð ¢ð ð ¢ð ð šð ¥ ð ‚ð «ð žð ° ð Ÿ ð Ÿ'ð Ÿ' ð“†ª",
+        vignette : fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        type de média : 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
